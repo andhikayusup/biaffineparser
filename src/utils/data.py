36c660956ab20f74e67fe4641e67467cb0bf9d36
andhikayusup <andeeka.ym@gmail.com>
2022-03-11 10:53:08 +0700

    feat: add fasttext embedding loader

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict, UserDict
 from os import PathLike
+import io
 from typing import (
     Callable,
     Collection,
@@ -205,3 +206,12 @@ def load_embeddings(
             raise ValueError(f"duplicate entry: {token!r}")
         embeddings[token] = [float(v) for v in vector]
     return embeddings
+
+def load_fasttext(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+    return data
